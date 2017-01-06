bfb351ea6816415323ccbf5661f6197537677aee
Phaneendra Kumar Kasalanati <phaneendrak.5757@gmail.com>
2017-01-06 12:32:22 +0530

    complete project

---
+++ app/src/main/java/org/learnprograming/screenmirroring/MainActivity.java
@@ -0,0 +1,36 @@
+package org.learnprograming.screenmirroring;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button server,clint;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        server=(Button) findViewById(R.id.server);
+        clint=(Button) findViewById(R.id.clint);
+
+        server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,MainServer.class);
+                startActivity(i);
+            }
+        });
+        clint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,MainClint.class);
+                startActivity(i);
+            }
+        });
+    }
+}
